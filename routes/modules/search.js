6822ca7df44c9d79ef292096d6b871db4e21272f
goater <goater1095@gmail.com>
2021-01-15 03:40:41 +0800

    modify index page & delete dialog(by onsubmit)

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const Rest = require('../../models/restaurant')
+const express = require('express');
+const router = express.Router();
+const Rest = require('../../models/restaurant');
 
 //set route
 router.get('/', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase()
-  Rest.find()
+  const userId = req.user._id;
+  const keyword = req.query.keyword.toLowerCase();
+  Rest.find({ userId })
     .lean()
     .then((rests) => {
       const searchResult = rests.filter(
-        (rest) => rest.name.toLowerCase().includes(keyword) && rest.category.toLowerCase().includes(keyword)
-      )
-      res.render('index', { resList: searchResult, keyword })
-      console.log(location.href)
+        (rest) =>
+          rest.name.toLowerCase().includes(keyword) ||
+          rest.category.toLowerCase().includes(keyword)
+      );
+      res.render('index', { resList: searchResult, keyword });
     })
-    .catch((error) => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-module.exports = router
+module.exports = router;
--- routes/modules/sort.js
+++ routes/modules/sort.js
@@ -5,9 +5,10 @@ const Rest = require('../../models/restaurant');
 router.get('/:keyword', (req, res) => {
   //為什麼 window 未定義???? (這是因為nodejs是後端的程式語言，無法拿到前端window的object。只有在寫真正前端的javascript才有window.)
   // console.log(window.location);
+  const userId = req.user._id;
   const keyword = req.params.keyword;
   let [method, select] = keyword.split('-');
-  Rest.find()
+  Rest.find({ userId })
     .lean()
     .sort({ [select]: [method] })
     .then((rests) => res.render('index', { resList: rests }))
